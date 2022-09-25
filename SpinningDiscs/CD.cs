ae4354e55b4451f6693834d812474dda4683ec93
saranell <saranell92@gmail.com>
2022-09-25 16:03:52 -0500

    Studio done

--- SpinningDiscs/CD.cs
+++ SpinningDiscs/CD.cs
@@ -1,11 +1,23 @@
 ﻿using System;
 namespace SpinningDiscs
 {
-    public class CD
+    public class CD : BaseDisc, IOpticalDisc
     {
         // TODO: Implement your custom interface.
 
         // TODO: Determine which fields, methods, and constructors can be extended from the base class and which ones
-        //  need to be declared separately. 
+        //  need to be declared separately.
+        public CD (string name, int maxCapacity, string diskType, int someUsedCapacity) : base (name, maxCapacity, diskType, someUsedCapacity)
+        {
+        }
+        public void ReadData()
+        {
+            Console.WriteLine("Wanna play a game?");
+        }
+
+        public void SpinDisc()
+        {
+            Console.WriteLine("A CD spins at the rate of 200 - 500 rpm.");
+        }
     }
 }
